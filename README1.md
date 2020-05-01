e6c6989e2816aeb8b864245b595bb9f0ef56a685
AluruRavindraBabu <AluruRavindraBabu@users.noreply.github.com>
2020-05-01 15:40:48 +0530

    commit project version1"

--- README1.md
+++ README1.md
@@ -2,3 +2,6 @@
 
 Retrieving, Processing, and Visualizing Data with Python
 
+Develop and Learning Blog project using Django , Apache Webserver, Tomcat Application Server and MySql Server
+
+This tutorial is only for my self development
---
+++ ivarblogproject/blog/models.py
@@ -0,0 +1,25 @@
+from django.db import  models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.urls import reverse
+# Create your models here.
+class CustomManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
+
+class Post(models.Model):
+    STATUS_CHOICES=(('draft','Draft'),('published','Published'))
+    title=models.CharField(max_length=264)
+    slug=models.SlugField(max_length=264,unique_for_date='publish')
+    author=models.ForeignKey(User,related_name='blog_posts',on_delete=models.CASCADE)
+    body=models.TextField()
+    publish=models.DateTimeField(default=timezone.now)
+    created=models.DateTimeField(auto_now_add=True)#datetime of create() action
+    updated=models.DateTimeField(auto_now=True)#datetime of save() action
+    status=models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
+    objects=CustomManager()
+
+    class Meta:
+        ordering=('-publish',)
+    def __str__(self):
+        return self.tit
